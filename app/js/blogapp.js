6aed5f3d0e715bc90d4bc4409626b03364c324a1
rfriedman <friedman.richard@gmail.com>
2016-11-09 12:03:45 -0500

    ready for next step

--- app/js/blogapp.js
+++ app/js/blogapp.js
@@ -1,15 +1,50 @@
-angular.module('blogApp', [])
+'use strict';
+// Make sure to include the `ui.router` module as a dependency
+angular.module('blogApp', [
+    'ui.router', 
+  'ngAnimate','siteService','siteControllers'
+])
 
+.run( 
+      ['$rootScope', '$state', '$stateParams',
+    function ($rootScope,   $state,   $stateParams) {
 
-  .controller('blogController',['$scope', function($scope) {
-    $scope.blogPost = {
-      title: 'blogPost.title',
-      meta:'blogPost.meta',
-      author: 'blogPost.author',
-      body: 'blogPost.body'
-    };
-    
-    
-  }]);
+    $rootScope.$state = $state;
+    $rootScope.$stateParams = $stateParams;
+    }
+  ]
+)
 
+.config(['$stateProvider', '$urlRouterProvider','siteConfigProvider',
+    function ($stateProvider,$urlRouterProvider,siteConfigProvider) {
+      $urlRouterProvider
+        .otherwise('/');
 
+      $stateProvider
+        .state("home", {
+          // Use a url of "/" to set a states as the "index".
+          url: "/",
+          templateUrl: 'partials/jumbotron.html' 
+        })
+
+      .state('about', {
+          url: '/about',
+          templateUrl: 'partials/about.html'
+        })
+        
+        .state('contacts', {
+          url: '/contacts',
+          templateUrl: 'partials/contacts.html'
+        })
+
+        .state('article',{
+          url: '/article',
+          templateUrl:'partials/article.html'
+        })
+
+        var siteConfig = {"siteName":"name of site", "siteID":5084220, "baseUrl":'json/site.json'};
+        siteConfigProvider.siteSettings(siteConfig);
+
+    }
+  ]
+);
